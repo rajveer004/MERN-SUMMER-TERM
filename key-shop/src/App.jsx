22a7291bba1495973199951ef45ee5f7845f0b27
rajveer004 <jaatrajveer2004@gmail.com>
2026-06-12 20:14:33 +0530

    starting of project

---
+++ key-shop/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header.jsx"
+
+function App(){
+  return(    
+    <div>
+       <Header/>
+       <h1>Hello</h1>
+       <p>hhhhhhhhh</p>
+    </div>
+    
+  )
+}export default App;
---
+++ key-shop/src/Header.jsx
@@ -0,0 +1,7 @@
+function Header(){
+    return (
+    <header>
+       <h1>i am header</h1>
+    </header>
+    )
+}export default Header;
